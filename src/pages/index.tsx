d18c8abe47b9fb7e08272d5959de3de24df9970f
Adriano <adrianoo.rdg@gmail.com>
2022-12-18 16:33:42 -0300

    feat: começando a estrutura da landing page com a tag main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,22 @@ import logo from "../assets/logo.svg";
 
 export default function Home() {
   return (
-    <header>
-      <Image src={logo} alt="Logo da Equipe Decola"></Image>
-      <nav>
-        <ul>
-          <li>Início</li>
-          <li>Sobre</li>
-          <li>Serviços</li>
-          <li>Depoimentos</li>
-        </ul>
-      </nav>
-      <button type="button">FAÇA O SEU ORÇAMENTO</button>
-    </header>
+    <>
+      <header>
+        <Image src={logo} alt="Logo da Equipe Decola"></Image>
+        <nav>
+          <ul>
+            <li>Início</li>
+            <li>Sobre</li>
+            <li>Serviços</li>
+            <li>Depoimentos</li>
+          </ul>
+        </nav>
+        <button type="button">FAÇA O SEU ORÇAMENTO</button>
+      </header>
+      <main>
+        <h1>Marketing simplicado para a sua empresa.</h1>
+      </main>
+    </>
   );
 }
