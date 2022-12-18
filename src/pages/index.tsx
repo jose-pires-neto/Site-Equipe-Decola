6c0c5e8e25b8b6591f3437abdef5dec1b2684144
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-12-18 12:16:46 -0300

    :sparkles: feat(Home): implementa a estrutura inicial do header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import logo from "../assets/logo.svg";
+
 export default function Home() {
-  return <h1>Decola</h1>;
+  return (
+    <header>
+      <Image src={logo} alt="Logo da Equipe Decola"></Image>
+      <nav>
+        <ul>
+          <li>Início</li>
+          <li>Sobre</li>
+          <li>Serviços</li>
+          <li>Depoimentos</li>
+        </ul>
+      </nav>
+      <button type="button">FAÇA O SEU ORÇAMENTO</button>
+    </header>
+  );
 }
