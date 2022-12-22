892976aed9b4024257abdca10a58c9c3d563e477
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-12-22 19:03:38 -0300

    feat(Hero): adiciona Hero e gif provisório

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import { HeroStyle } from "./heroStyle";
+import test from "../../assets/gif1.gif";
+
 export default function Hero() {
   return (
-    <>
-      <h1>Hero</h1>
-    </>
+    <HeroStyle>
+      <Image src={test} alt="gif de pessoas em uma reunião"></Image>
+    </HeroStyle>
   );
 }
