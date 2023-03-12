ece2736a70cc68fc8d5bf90f6b8cbb3fa0670a32
jovemcleb <lima.cardoso.caleb@gmail.com>
2023-03-12 15:11:55 -0300

    refactor(team): implementa map nos cards

--- src/components/Team/team.tsx
+++ src/components/Team/team.tsx
@@ -1,17 +1,62 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { CardsContainer, TeamContainer, Card } from './teamStyle';
 
 import jp from '../../assets/jp.png';
-import goreh from '../../assets/adriano.png';
+import adriano from '../../assets/adriano.png';
 import caleb from '../../assets/cleb.png';
 
-import black from '../../assets/vetorBlack.svg';
+import vectorBlack from '../../assets/vetorBlack.svg';
 import vetorBlue from '../../assets/vetorBlue.svg';
 import vetorGreen from '../../assets/vetorGreen.svg';
 
 import linkedin from '../../assets/icons/linkedin.svg';
 import github from '../../assets/icons/github.svg';
 import Link from 'next/link';
+
+type CardProps = {
+  background: 'black' | 'blue' | 'green';
+  vector: StaticImageData;
+  imageProfile: StaticImageData;
+  description: string;
+  name: string;
+  role: string;
+  linkedinUrl: string;
+  githubUrl: string;
+};
+
+const cards: CardProps[] = [
+  {
+    background: 'black',
+    vector: vectorBlack,
+    imageProfile: jp,
+    description: 'Foto do CEO da Equipe Decola',
+    name: 'José Pires',
+    role: 'Chief Executive Officer (CEO)',
+    linkedinUrl:
+      'https://www.linkedin.com/in/jos%C3%A9-pires-oliveira-a97430237/',
+    githubUrl: 'https://github.com/jose-pires-neto',
+  },
+  {
+    background: 'blue',
+    vector: vetorBlue,
+    imageProfile: caleb,
+    description: 'Foto do desenvolvedor Front-end',
+    name: 'Caleb Lima',
+    role: 'Front-end Developer',
+    linkedinUrl: 'https://www.linkedin.com/in/caleb-lima/',
+    githubUrl: 'https://github.com/jovemcleb',
+  },
+  {
+    background: 'green',
+    vector: vetorGreen,
+    imageProfile: adriano,
+    description: 'Foto do desenvolvedor Back-end',
+    name: 'Adriano Santos',
+    role: 'Back-end Developer',
+    linkedinUrl: 'https://www.linkedin.com/in/adriano-rdg/',
+    githubUrl: 'https://github.com/AdrianoRdg',
+  },
+];
 
 export default function Team() {
   return (
@@ -23,96 +68,39 @@ export default function Team() {
         personalizada para atingir seus objetivos.
       </p>
       <CardsContainer>
-        <Card background={'black'} data-aos="zoom-in-up">
-          <Image src={black} alt="" className="vector" />
-          <Image
-            src={jp}
-            alt="Foto do CEO da Equipe Decola"
-            className="imageProfile"
-            placeholder="blur"
-          />
-          <h2>José Pires</h2>
-          <span>Chief Executive Officer (CEO)</span>
-          <div>
-            <Link
-              href="https://www.linkedin.com/in/jos%C3%A9-pires-oliveira-a97430237/"
-              target="_blank"
-            >
-              <Image
-                src={linkedin}
-                alt="Ícone do Linkedin"
-                className="socialIcons"
-              />
-            </Link>
-            <Link href="https://github.com/jose-pires-neto" target="_blank">
-              <Image
-                src={github}
-                alt="Ícone do Github"
-                className="socialIcons"
-              />
-            </Link>
-          </div>
-        </Card>
-        <Card background={'blue'} data-aos="zoom-in-up">
-          <Image src={vetorBlue} alt="" className="vector" />
-          <Image
-            src={caleb}
-            alt="Foto do desenvolvedor Front-end"
-            className="imageProfile"
-            placeholder="blur"
-          />
-          <h2>Caleb Lima</h2>
-          <span>Front-end Developer</span>
-          <div>
-            <Link
-              href="https://www.linkedin.com/in/caleb-lima/"
-              target="_blank"
-            >
-              <Image
-                src={linkedin}
-                alt="Ícone do Linkedin"
-                className="socialIcons"
-              />
-            </Link>
-            <Link href="https://github.com/jovemcleb" target="_blank">
-              <Image
-                src={github}
-                alt="Ícone do Github"
-                className="socialIcons"
-              />
-            </Link>
-          </div>
-        </Card>
-        <Card background={'green'} data-aos="zoom-in-up">
-          <Image src={vetorGreen} alt="" className="vector" />
-          <Image
-            src={goreh}
-            alt="Foto do desenvolvedor Back-end"
-            className="imageProfile"
-            placeholder="blur"
-          />
-          <h2>Adriano Rodrigues</h2>
-          <span>Back-end Developer</span>
-          <div>
-            <Link
-              href="https://www.linkedin.com/in/adriano-rdg/"
-              target="_blank"
-            >
-              <Image
-                src={linkedin}
-                alt="Ícone do Linkedin"
-                className="socialIcons"
-              />
-            </Link>
-            <Link href="https://github.com/AdrianoRdg" target="_blank">
-              <Image
-                src={github}
-                alt="Ícone do Github"
-                className="socialIcons"
-              />
-            </Link>
-          </div>
-        </Card>
+        {cards.map((card) => (
+          <Card
+            key={card.name}
+            background={card.background}
+            data-aos="zoom-in-up"
+          >
+            <Image src={card.vector} alt="" className="vector" />
+            <Image
+              src={card.imageProfile}
+              alt={card.description}
+              className="imageProfile"
+              placeholder="blur"
+            />
+            <h2>{card.name}</h2>
+            <span>{card.role}</span>
+            <div>
+              <Link href={card.linkedinUrl} target="_blank">
+                <Image
+                  src={linkedin}
+                  alt="Ícone do Linkedin"
+                  className="socialIcons"
+                />
+              </Link>
+              <Link href={card.githubUrl} target="_blank">
+                <Image
+                  src={github}
+                  alt="Ícone do Github"
+                  className="socialIcons"
+                />
+              </Link>
+            </div>
+          </Card>
+        ))}
       </CardsContainer>
     </TeamContainer>
   );
