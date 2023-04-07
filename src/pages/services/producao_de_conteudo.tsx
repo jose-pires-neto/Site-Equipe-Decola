e6e61984a1f2b8571ff407300f10e0f9a4fcebb5
jovemcleb <lima.cardoso.caleb@gmail.com>
2023-04-07 16:37:57 -0300

    feat: adiciona title em pages de service e um transition no Header

--- src/pages/services/producao_de_conteudo.tsx
+++ src/pages/services/producao_de_conteudo.tsx
@@ -13,61 +13,68 @@ import engajamentoIcon from '../../assets/icons/producaoDeConteudoIcons/engajame
 import SEOIcon from '../../assets/icons/producaoDeConteudoIcons/SEOIcon.svg';
 import autoridadeIcon from '../../assets/icons/producaoDeConteudoIcons/autoridadeIcon.svg';
 import leadsIcon from '../../assets/icons/producaoDeConteudoIcons/leadsIcon.svg';
+import Head from 'next/head';
 
 export default function ProducaoDeConteudo() {
   return (
-    <ServiceSection>
-      <ServiceContainer>
-        <h1>Produção de conteúdo</h1>
-        <hr />
-        <TextAndImageContainer>
-          <Image
-            src={ProducaoDeConteudoImage}
-            alt="producao-de-conteudo-image"
-            placeholder="blur"
-          />
-          <div>
+    <>
+      <Head>
+        <title>Produção de conteúdo | Equipe Decola</title>
+      </Head>
+      <ServiceSection>
+        <ServiceContainer>
+          <h1>Produção de conteúdo</h1>
+          <hr />
+          <TextAndImageContainer>
+            <Image
+              src={ProducaoDeConteudoImage}
+              alt="producao-de-conteudo-image"
+              placeholder="blur"
+            />
+            <div>
+              <p>
+                A produção de conteúdo é uma parte importante do marketing, pois
+                permite que as empresas comuniquem sua mensagem a um
+                público-alvo específico de maneira criativa e eficaz. O conteúdo
+                pode ser produzido em vários formatos, incluindo textos,
+                imagens, vídeos, áudio e muito mais.
+              </p>
+            </div>
+          </TextAndImageContainer>
+          <ContentContainer>
             <p>
-              A produção de conteúdo é uma parte importante do marketing, pois
-              permite que as empresas comuniquem sua mensagem a um público-alvo
-              específico de maneira criativa e eficaz. O conteúdo pode ser
-              produzido em vários formatos, incluindo textos, imagens, vídeos,
-              áudio e muito mais.
+              Existem várias razões pelas quais a sua empresa precise de
+              produção de conteúdo. Algumas das principais razões incluem:
             </p>
-          </div>
-        </TextAndImageContainer>
-        <ContentContainer>
-          <p>
-            Existem várias razões pelas quais a sua empresa precise de produção
-            de conteúdo. Algumas das principais razões incluem:
-          </p>
-          <IconsContainer>
-            <div>
-              <Image src={engajamentoIcon} alt="engajamento-icon" />
-              <h3>Engajamento ao público</h3>
-            </div>
-            <div>
-              <Image src={SEOIcon} alt="SEO-icon" />
-              <h3>SEO</h3>
-            </div>
-            <div>
-              <Image src={leadsIcon} alt="leads-icon" />
-              <h3>Geração de leads</h3>
-            </div>
-            <div>
-              <Image src={autoridadeIcon} alt="autoridade-icon" />
-              <h3>Autoridade</h3>
-            </div>
-          </IconsContainer>
-          <p>
-            Nossa equipe é composta por profissionais experientes e criativos
-            que trabalham em estreita colaboração com nossos clientes para criar
-            conteúdo de alta qualidade que seja relevante para seu público-alvo
-            e que ajude a estabelecer sua marca como líder em seu campo.
-          </p>
-        </ContentContainer>
-        <BudgetButton />
-      </ServiceContainer>
-    </ServiceSection>
+            <IconsContainer>
+              <div>
+                <Image src={engajamentoIcon} alt="engajamento-icon" />
+                <h3>Engajamento ao público</h3>
+              </div>
+              <div>
+                <Image src={SEOIcon} alt="SEO-icon" />
+                <h3>SEO</h3>
+              </div>
+              <div>
+                <Image src={leadsIcon} alt="leads-icon" />
+                <h3>Geração de leads</h3>
+              </div>
+              <div>
+                <Image src={autoridadeIcon} alt="autoridade-icon" />
+                <h3>Autoridade</h3>
+              </div>
+            </IconsContainer>
+            <p>
+              Nossa equipe é composta por profissionais experientes e criativos
+              que trabalham em estreita colaboração com nossos clientes para
+              criar conteúdo de alta qualidade que seja relevante para seu
+              público-alvo e que ajude a estabelecer sua marca como líder em seu
+              campo.
+            </p>
+          </ContentContainer>
+          <BudgetButton />
+        </ServiceContainer>
+      </ServiceSection>
+    </>
   );
 }
--- src/pages/services/social_media.tsx
+++ src/pages/services/social_media.tsx
@@ -8,42 +8,48 @@ import Image from 'next/image';
 import BudgetButton from '../../components/BudgetButton/budgetButton';
 
 import SocialMediaImage from '../../assets/pages/socialMediaImage.png';
+import Head from 'next/head';
 
 export default function SocialMedia() {
   return (
-    <ServiceSection>
-      <ServiceContainer>
-        <h1>Social Media</h1>
-        <hr />
-        <TextAndImageContainer>
-          <Image
-            src={SocialMediaImage}
-            alt="social-media-image"
-            placeholder="blur"
-          />
-          <div>
+    <>
+      <Head>
+        <title>Social Media | Equipe Decola</title>
+      </Head>
+      <ServiceSection>
+        <ServiceContainer>
+          <h1>Social Media</h1>
+          <hr />
+          <TextAndImageContainer>
+            <Image
+              src={SocialMediaImage}
+              alt="social-media-image"
+              placeholder="blur"
+            />
+            <div>
+              <p>
+                O social media é uma parte importante da estratégia de marketing
+                digital de uma empresa, pois permite que ela atinja um público
+                amplo e segmentado, além de fornecer uma plataforma para medir o
+                sucesso das campanhas e ajustar as estratégias de acordo com os
+                resultados obtidos.
+              </p>
+            </div>
+          </TextAndImageContainer>
+          <ContentContainer>
             <p>
-              O social media é uma parte importante da estratégia de marketing
-              digital de uma empresa, pois permite que ela atinja um público
-              amplo e segmentado, além de fornecer uma plataforma para medir o
-              sucesso das campanhas e ajustar as estratégias de acordo com os
-              resultados obtidos.
+              Social media é o uso das redes sociais como uma plataforma para
+              promover um negócio ou marca e atrair novos clientes. Isso pode
+              incluir a criação de conteúdo e publicações em redes sociais, a
+              realização de campanhas publicitárias paga em plataformas como o
+              Facebook e o Instagram, o uso de hashtags para aumentar a
+              visibilidade e o envolvimento dos seguidores, bem como a interação
+              com os clientes através de comentários e mensagens privadas.
             </p>
-          </div>
-        </TextAndImageContainer>
-        <ContentContainer>
-          <p>
-            Social media é o uso das redes sociais como uma plataforma para
-            promover um negócio ou marca e atrair novos clientes. Isso pode
-            incluir a criação de conteúdo e publicações em redes sociais, a
-            realização de campanhas publicitárias paga em plataformas como o
-            Facebook e o Instagram, o uso de hashtags para aumentar a
-            visibilidade e o envolvimento dos seguidores, bem como a interação
-            com os clientes através de comentários e mensagens privadas.
-          </p>
-        </ContentContainer>
-        <BudgetButton />
-      </ServiceContainer>
-    </ServiceSection>
+          </ContentContainer>
+          <BudgetButton />
+        </ServiceContainer>
+      </ServiceSection>
+    </>
   );
 }
